adb284acfd93680117a7910e93919ee4b0a44814
katieadamsky <adamsky.3@osu.edu>
2017-11-20 16:15:07 -0500

    more space-efficient version of permutation generation, so maybe it won't get killed now

---
+++ bruteforce.py
@@ -0,0 +1,74 @@
+'''
+ Script to brute force decrypt a cipher that is first enciphered with playfair, then with 
+ columnar transposition. Use a multithreaded approach to test multiple attempts at once.
+
+'''
+
+import thread
+from pycipher import ColTrans
+from pycipher import Playfair
+import itertools
+import math
+
+# trash
+def swap(text, ch1, ch2):
+	text = text.replace(ch2, '!',)
+	text = text.replace(ch1, ch2)
+	text = text.replace('!', ch1)
+	return text
+
+# check if plaintext contains the cribs
+def check(plaintext):
+	if 'congressman' in plaintext or 'foolproof' in plaintext or 'liberation' in plaintext:
+		return True
+	else:
+		return False
+
+# checks if the given text is valid playfair ciphertext
+def can_decipher(pf_text):
+	# invalid playfair ciphertext will have pairs with duplicate letters
+	text = list(pf_text)
+	i = 0
+	while (i < len(text)):
+		if text[i] == text[i+1]:
+			return False
+		i += 2
+	return True
+
+
+ciphertext = 'mhstbtyifsaddntphrmlrdktohbobtpaztnfcqmqddsqcdxtermcsmbimmundckladrhcmcsmpbnbivhcmhmftmxtcthmcuepeucphhskrrrcdysgmmglezynfsxylmiaicetnqfwnnunqprcwbccfmtcsprkmctxgrtbqmrkyaxvqzgyanhwnahoqxfgmucnymmogkngnbtsfyddbwhroyanmdaxcizrqnweqqtbffszayoaibirshchsqfhdoqckecpereemakofrcxynlicqonnmiezfqxeecdcerlvsyzdytlcsikrlxccauiecmnvchkedgckhrnuyafuyuklrdzidkyhkeelcoonlxwfdcnddninqhacoxrbxnkkcbqemrknbcymeygzrgmdesizmqxkirairednaafiudmulioiimueezdxccdamhruothtdscmhksvheeehhesidyoklaqqbzstmystbxcieuuuicruaamqxqlqrmzhdsuysuxqmmzmemmqxkqfurmeefiempdnkukrgtofmnwgirmxodaruupxseceehoedrfeprsdiehethxaixtyeuqimneckyelccdmuytfcnnmznngoemcptciobphomckmncmceevzpcamosktzdccaxdhlihppemddqtxrhohkksrxhhkmixuvmicmxdqoeahumqzslcakhstqphippxuppchddtcmqyhnasoxpeumligmkeimpfqoacqplresdxscdfcdlcehhhcmdhbmuoqzapaccttmqrmpzeakcgsagpqsccitsfmmfoqmcfxeiirkdcmglhdmumixmfrpahvuaeuhmgncikhmpcxiplnqmplhcctpcqpxemskcecsdcmcuvqcsiseimtvmklsdcaimyhciqpqedahcycudahmxvxcqqurtgerchwfwdttcrecsebivrfrnqicnkyerqvnymyckpnsxgdepgsbmmrazhclcccqhsyhgcxymkyvrndigytcbgcnecshxfnctkeecpn'
+
+# Start 2 threads, each of which will attempt a different possible ct key length
+
+# try:
+#    thread.start_new_thread( generate_key, ("Thread-1", 2, ) )
+#    thread.start_new_thread( generate_key, ("Thread-2", 4, ) )
+# except:
+#    print "Error: unable to start thread"
+
+plaintext = ''
+
+# key length is not 6, as none of them generate valid playfair text
+# try 9 - doesn't work
+# try 12 - hypothesis: key is length 12
+# all_ct_keys = permute('abcdefghiklm')
+possibilities = 0
+
+word = 'abcdefghij'
+
+perm = list(word)
+weights = [0] * len(word)
+upper = 1
+while upper < len(perm):
+	if weights[upper] < upper:
+		lower = 0 if (upper % 2 == 0) else weights[upper]
+		perm = swap(''.join(perm), perm[upper], perm[lower])
+		pf_text = ColTrans(perm).decipher(ciphertext)
+		if can_decipher(pf_text):
+			print perm
+		perm = list(perm)
+		weights[upper] += 1
+		upper = 1
+	else:
+		weights[upper] = 0
+		upper += 1
